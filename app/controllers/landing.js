7a91ef70fa2dde0d1a3598cf1d120a3ee9a8463b
Romserg <serg.romankov@gmail.com>
2019-07-21 22:22:24 +0300

    Implement deleting lead

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -49,6 +49,7 @@ exports.edit_lead = function (req, res, next) {
       res.redirect('/lead/' + req.params.lead_id);
     })
 };
+
 exports.delete_lead = function (req, res, next) {
   return models.Lead.destroy({
     where: {
@@ -56,5 +57,15 @@ exports.delete_lead = function (req, res, next) {
     }
   }).then(result => {
     res.redirect('/leads');
+  })
+};
+
+exports.delete_lead_json = function (req, res, next) {
+  return models.Lead.destroy({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(result => {
+    res.send({ msg: "Success" });
   })
 };
