3af5097fac3aa4d4c5971f57b9a281c59a4c1867
Romserg <serg.romankov@gmail.com>
2019-07-22 00:29:31 +0300

    Implement a lead delete route using ajax

--- app/public/javascripts/lead.js
+++ app/public/javascripts/lead.js
@@ -1,12 +1,12 @@
 function deleteLead(leadId) {
   $.ajax({
-    url: '/lead/' + lead.id + '/delete-json',
+    url: '/lead/' + leadId + '/delete-json',
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     data: JSON.stringify({leadId}),
     type: 'POST',
     success: (res => {
-      console.log("Result: ", res)
+      console.log("Result: ", res);
       $("#" + leadId).remove();
     }),
     error: (err => {
